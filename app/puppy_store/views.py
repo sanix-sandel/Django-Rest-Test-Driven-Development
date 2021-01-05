20c58e037bebae6fed29b02167b7a5ee52786604
sanix <sanicksikani@gmail.com>
2021-01-06 01:32:07 +0300

    Create Puppy test passed

--- app/puppy_store/views.py
+++ app/puppy_store/views.py
@@ -30,4 +30,14 @@ def get_post_puppies(request):
         return Response(serializer.data)
     # insert a new record for a puppy
     elif request.method == 'POST':
-        return Response({})
+        data = {
+            'name': request.data.get('name'),
+            'age': int(request.data.get('age')),
+            'breed': request.data.get('breed'),
+            'color': request.data.get('color')
+        }
+        serializer = PuppySerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
