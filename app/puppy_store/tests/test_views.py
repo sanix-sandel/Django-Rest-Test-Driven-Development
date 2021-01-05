cb2075b4a980e314cf580372718b2b316c9180dc
sanix <sanicksikani@gmail.com>
2021-01-06 01:45:17 +0300

    PUT test passed

--- app/puppy_store/tests/test_views.py
+++ app/puppy_store/tests/test_views.py
@@ -86,3 +86,39 @@ class CreateNewPuppyTest(TestCase):
             content_type='application/json'
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+class UpdateSinglePuppyTest(TestCase):
+    """ Test module for updating an existing puppy record """
+
+    def setUp(self):
+        self.casper = Puppy.objects.create(
+            name='Casper', age=3, breed='Bull Dog', color='Black')
+        self.muffin = Puppy.objects.create(
+            name='Muffy', age=1, breed='Gradane', color='Brown')
+        self.valid_payload = {
+            'name': 'Muffy',
+            'age': 2,
+            'breed': 'Labrador',
+            'color': 'Black'
+        }
+        self.invalid_payload = {
+            'name': '',
+            'age': 4,
+            'breed': 'Pamerion',
+            'color': 'White'
+        }
+
+    def test_valid_update_puppy(self):
+        response = client.put(
+            reverse('get_delete_update_puppy', kwargs={'pk': self.muffin.pk}),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_invalid_update_puppy(self):
+        response = client.put(
+            reverse('get_delete_update_puppy', kwargs={'pk': self.muffin.pk}),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- app/puppy_store/views.py
+++ app/puppy_store/views.py
@@ -18,7 +18,11 @@ def get_delete_update(request, pk):
         return Response({})
     # update details of a single puppy
     elif request.method == 'PUT':
-        return Response({})               
+        serializer = PuppySerializer(puppy, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)            
 
 
 @api_view(['GET', 'POST'])
@@ -41,3 +45,6 @@ def get_post_puppies(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+        
